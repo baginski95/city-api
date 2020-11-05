ed433d4e67964a97a7fc89624055765f237e0ed1
baginski95 <bagiczny3@gmail.com>
2020-11-05 23:31:19 +0100

    Created main controler

---
+++ CityInfo/CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Api.Controllers
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>()
+            {
+                new {id = 1, name = "Warsaw"},
+                new {id = 2, name = "Cracow"}
+            });
+        }
+    }
+}
