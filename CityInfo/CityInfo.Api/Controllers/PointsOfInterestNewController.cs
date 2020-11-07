d49002d59d523865246f6baab2d2d5e4fef0993a
baginski95 <bagiczny3@gmail.com>
2020-11-07 23:59:54 +0100

    New controller for pointOfInterest

---
+++ CityInfo/CityInfo.Api/Controllers/PointsOfInterestNewController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers
+{
+    public class PointsOfInterestNewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
