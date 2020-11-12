4c38eb48bb729545f0956d9137045bcd3bf0e6e8
baginski95 <bagiczny3@gmail.com>
2020-11-12 16:58:36 +0100

    Created 1st httpPost request for creating new pointofinterest

--- CityInfo/CityInfo.Api/Controllers/PointsOfInterestController.cs
+++ CityInfo/CityInfo.Api/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.Api.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,28 @@ namespace CityInfo.Api.Controllers
                 return NotFound();
             }
             return Ok(city.PointsOfInterest);
+
+        }
+        [HttpPost]
+        public IActionResult CreatePointOfInterest(int cityId, 
+            [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                c => c.PointsOfInterest).Max( p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
 
         }
     }
