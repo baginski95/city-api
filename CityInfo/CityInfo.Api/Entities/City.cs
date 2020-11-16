1c36a7b6c3007c542f86587919e60b5b9f034207
baginski95 <bagiczny3@gmail.com>
2020-11-16 13:26:02 +0100

    Created City and PointOfInterest classes for EF Core

---
+++ CityInfo/CityInfo.Api/Entities/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Api.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+         = new List<PointOfInterestDto>();
+    }
+}
+}
