9f0441620292d48807ee735dae13f7e494480568
baginski95 <bagiczny3@gmail.com>
2020-11-17 08:48:58 +0100

    Created annotations for City and PointOfInterest in Entities

--- CityInfo/CityInfo.Api/Entities/City.cs
+++ CityInfo/CityInfo.Api/Entities/City.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +9,18 @@ namespace CityInfo.Api.Entities
 {
     public class City
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+        [MaxLength(200)]
         public string Description { get; set; }
 
 
-        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
-         = new List<PointOfInterestDto>();
+        public ICollection<PointOfInterest> PointsOfInterest { get; set; }
+         = new List<PointOfInterest>();
     }
 }
 }
--- CityInfo/CityInfo.Api/Entities/PointOfInterest.cs
+++ CityInfo/CityInfo.Api/Entities/PointOfInterest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace CityInfo.Api.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        [ForeignKey("CityId")]
+        public City city { get; set; }
+        public int CityId { get; set; }
     }
 }
